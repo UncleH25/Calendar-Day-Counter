05fc6ed1e936c1e08906fc6c2542e1a569772b69
UncleH25 <oneilljarryd@gmail.com>
2025-06-05 05:42:57 +0200

    Added a switch case block to determine the number of daya in a inputted month, with month 2 being the one to check for a leap year.

--- Calendar Day Counter.cpp
+++ Calendar Day Counter.cpp
@@ -29,6 +29,28 @@ int main()
 		//Leap year check
 		bool isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 
+		//Determine the number of days in the month
+		switch (month) {
+			case 1: // January
+			case 3: // March
+			case 5: // May
+			case 7: // July
+			case 8: // August
+			case 10: // October
+			case 12: // December
+				daysInMonth = 31;
+				break;
+			case 4: // April
+			case 6: // June
+			case 9: // September
+			case 11: // November
+				daysInMonth = 30;
+				break;
+			case 2: // February
+				daysInMonth = isLeapYear ? 29 : 28;
+				break;
+		}
+
 	} while (again == 'y' || again == 'Y');
 
     return 0;
